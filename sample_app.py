7e2339d4f65e7b2407eeeefe42bb1de0759f4639
yajun1133 <wu18366696275@163.com>
2026-05-20 12:47:59 +0000

    Changed files verification.

--- sample_app.py
+++ sample_app.py
@@ -9,5 +9,9 @@ sample = Flask(__name__)
 def main():
     return render_template("index.html")
 
+
 if __name__ == "__main__":
     sample.run(host="0.0.0.0", port=8080)
+
+
+echo "# Modification for lab screenshot" >> sample_app.py
